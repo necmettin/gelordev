1e5ec4754494677091f7cfca7b1c7500fb0f28f0
Necmettin Begiter <necmettin.begiter@gmail.com>
2022-10-09 14:03:55 +0300

    add carbon icons and carbon svelte preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
 import adapter from '@sveltejs/adapter-auto';
 
+import sveltePreprocess from "svelte-preprocess";
+import { optimizeImports } from "carbon-preprocess-svelte";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	preprocess: [
+		sveltePreprocess(),
+		optimizeImports(),
+	],
 };
 
 export default config;
